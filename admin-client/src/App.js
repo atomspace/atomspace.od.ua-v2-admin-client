d6c5858247b326d5ef992d21b623f1f511e8323c
Volodymyr Saakian <vsaakian@lohika.com>
2019-12-24 19:57:45 +0200

    Add admin-client

---
+++ admin-client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { LoginPage } from './routes/LoginPage/LoginPage';
+import './App.scss';
+
+function App() {
+  return (
+    <div>
+      <LoginPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ admin-client/src/routes/LoginPage/LoginPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+import LoginForm from '../../components/LoginForm/LoginForm';
+import './LoginPage.scss';
+
+export const LoginPage = () => (
+    <div className='login-page'>
+        <LoginForm/>
+    </div>
+);
