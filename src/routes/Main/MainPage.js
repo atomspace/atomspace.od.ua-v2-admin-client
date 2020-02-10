adac08b0763f819e32b9a426cfcf1e3de866e6d5
Mishawaka <you@example.com>
2020-02-10 09:18:33 +0200

    Fixed an infinite loop in useEffect hook

--- src/routes/Main/MainPage.js
+++ src/routes/Main/MainPage.js
@@ -10,31 +10,27 @@ import '../../assets/styles/MainPage.scss';
 export const MainPage = () => {
 	const [modal, setModal] = useState(false);
 
-	const { addMerches } = useContext(MerchContext);
+	const { addMerches, merches, loading } = useContext(MerchContext);
 	useEffect(() => {
 		fetch('http://localhost:8000/api/v1/get-all-merch')
 			.then(res => res.json())
 			.then(data => addMerches(data))
 			.catch(err => console.log(err));
 		return undefined;
-	});
+	}, [loading]);
 
 	return (
-		<MerchContext.Consumer>
-			{({ merches }) => (
-				<div className='container'>
-					<Sidebar />
-					<div className='main-content'>
-						<div className='add-item'><div className='add-img'><img src={plusImg} alt='Plus' /></div></div>
-						<div className='grid-items'>
-							{merches.map(merch => (
-								<GridItem key={merch.id} setModal={setModal} merch={merch} />
-							))}
-						</div>
-					</div>
-					<Modal modal={modal} setModal={setModal} />
+		<div className='container'>
+			<Sidebar />
+			<div className='main-content'>
+				<div className='add-item'><div className='add-img'><img src={plusImg} alt='Plus' /></div></div>
+				<div className='grid-items'>
+					{merches.map(merch => (
+						<GridItem key={merch.id} setModal={setModal} merch={merch} />
+					))}
 				</div>
-			)}
-		</MerchContext.Consumer>
+			</div>
+			<Modal modal={modal} setModal={setModal} />
+		</div>
 	);
 };
