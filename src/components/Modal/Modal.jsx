f07b62749ecfe71a7fd482e8e2a83312b263c110
Mishawaka <you@example.com>
2020-02-15 15:16:48 +0200

    For Vova's check

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -5,11 +5,12 @@ import { Loader } from '../Loader/Loader';
 
 import closeImg from '../../assets/img/close.svg';
 
-export const Modal = ({ edit, modal, setModal }) => {
+export const Modal = ({ modal, setModal, data }) => {
 	let className = modal ? 'modal-show' : 'modal-hide';
-
+	let { type } = data;
 	const { merchToEdit, setMerchToEdit, loading, setLoading } = useContext(MerchContext);
-	const [message, setMessage] = useState('');
+	const [newMerch, setNewMerch] = useState({ name: '', price: '' });
+	const [file, setFile] = useState('');
 
 	const editMerch = merch => {
 		setLoading(true);
@@ -21,9 +22,31 @@ export const Modal = ({ edit, modal, setModal }) => {
 			.then(res => res.json())
 			.then(data => {
 				setLoading(false);
-				setMessage('Changed successfully!!!');
-				setModal(false);
-				return setMessage('');
+				return setModal(false);
+			})
+			.catch(err => {
+				setLoading(false);
+				return setModal(false);
+			});
+		return null;
+	}
+
+	const addNewMerch = merch => {
+		setLoading(true);
+		let form = new FormData();
+		form.append('name', merch.name);
+		form.append('price', merch.price);
+		// form.append('photo', file);
+		console.log(file);
+		fetch('http://localhost:8000/api/v1/add-new-merch', {
+			method: 'POST',
+			// headers: { 'Content-Type': 'multipart/form-data' },
+			body: form
+		})
+			.then(res => res.json())
+			.then(data => {
+				setLoading(false);
+				return setModal(false);
 			})
 			.catch(err => {
 				setLoading(false);
@@ -36,18 +59,34 @@ export const Modal = ({ edit, modal, setModal }) => {
 		<div className={className}>
 			<div className='close-item'><div onClick={() => setModal(false)} className='add-img'><img src={closeImg} alt='Close' /></div></div>
 			<div className='form'>
-				<div className="auth-form">
-					<h2>Edit merch info</h2>
-					<input className='login' type="text"
-						onChange={e => setMerchToEdit({ ...merchToEdit, name: e.target.value })}
-						value={merchToEdit.name} placeholder='Merch Name' />
-					<input className='password' type="number"
-						onChange={e => setMerchToEdit({ ...merchToEdit, price: +e.target.value })}
-						value={merchToEdit.price} placeholder='Price' />
-					<button style={{ display: loading ? 'none' : 'block' }} onClick={() => editMerch(merchToEdit)} className='submit-input'>Edit</button>
-					<Loader loading={loading} />
-					<h2 style={{ display: message === '' ? 'none' : 'block' }}>{message}</h2>
-				</div>
+				{type === 'add' ? (
+					<div className="auth-form">
+						<h2>Add new merch</h2>
+						<input className='login' type="text"
+							onChange={e => setNewMerch({ ...newMerch, name: e.target.value })}
+							value={newMerch.name} placeholder='Merch Name' />
+						<input className='password' type="number"
+							onChange={e => setNewMerch({ ...newMerch, price: +e.target.value })}
+							value={newMerch.price} placeholder='Price' />
+						<input type="file" onChange={e => setFile(e.target.files[0])} id='merch-img' />
+						<button style={{ display: loading ? 'none' : 'block' }} onClick={() => addNewMerch(newMerch)} className='submit-input'>Add</button>
+						<Loader loading={loading} />
+					</div>
+				)
+					:
+					(
+						<div className="auth-form">
+							<h2>Edit merch info</h2>
+							<input className='login' type="text"
+								onChange={e => setMerchToEdit({ ...merchToEdit, name: e.target.value })}
+								value={merchToEdit.name} placeholder='Merch Name' />
+							<input className='password' type="number"
+								onChange={e => setMerchToEdit({ ...merchToEdit, price: +e.target.value })}
+								value={merchToEdit.price} placeholder='Price' />
+							<button style={{ display: loading ? 'none' : 'block' }} onClick={() => editMerch(merchToEdit)} className='submit-input'>Edit</button>
+							<Loader loading={loading} />
+						</div>
+					)}
 			</div>
 		</div>
 	);
